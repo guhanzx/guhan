069a47ba36fa4ec5f6956c3ecde84053b8f946d5
guhanzx <87961491+guhanzx@users.noreply.github.com>
2021-08-03 16:57:03 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Join now ♻️', url="https://t.me/Tamilblasters3k"
                                 )
                         ]
                     ]
